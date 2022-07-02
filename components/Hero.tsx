946649310b3bc7eb2b09883d417c22f7a232c5e9
udochukwuka onyela <udochukwukaonyela@gmail.com>
2022-07-02 17:22:11 +0100

    created hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,10 @@
+import { NextComponentType } from "next"
+import React from 'react'
+
+const Hero:NextComponentType = () => {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div>Home</div>
+    <div></div>
   )
 }
 
