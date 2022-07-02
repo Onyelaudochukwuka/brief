c6cabab12ce47a1b263b5c08fef4c95d9f84a96d
udochukwuka onyela <udochukwukaonyela@gmail.com>
2022-07-02 18:36:31 +0100

    desktop styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,20 +1,19 @@
 import { NextComponentType } from "next"
 import React from 'react';
-import Svg from './Svg';
 import Image from "next/image";
 import image from "../images/illustration-working.svg";
 const Hero:NextComponentType = () => {
   return (
     <div className="flex items-center p-3 lg:p-12">
-      <div className="">
+      <div className="flex flex-col gap-10">
         <div>
           <h1 className="text-6xl font-bold">More than just shorter links</h1>
-          <p>Build your brand's recognition and get detailed insights 
+          <p className="w-3/4 text-neutralGrayish text-lg">Build your brand's recognition and get detailed insights 
   on how your links are performing.</p>
         </div>
-        <span className="px-5 py-3 bg-primaryCol rounded-full text-white hover:bg-primaryCol/75 cursor-pointer">Get Started</span>
+        <span className="px-8 py-3 bg-primaryCol rounded-full text-white hover:bg-primaryCol/75 cursor-pointer w-fit">Get Started</span>
       </div>
-      <div className="w-full h-auto block p-0 m-0"><Image src={image} layout="responsive" /></div>
+      <div className="w-3/4 h-auto block p-0 m-0"><Image src={image} layout="responsive" /></div>
     </div>
   )
 }
