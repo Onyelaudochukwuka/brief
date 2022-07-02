679ca4deffff205ad2c592b9123e68f7efc7b660
udochukwuka onyela <udochukwukaonyela@gmail.com>
2022-07-02 20:36:56 +0100

    imported icons

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import Search from "./Search"
-
+import Image from "next/image"
+import icon1 from '../images/icon-brand-recognition.svg';
+import icon2 from '../images/icon-detailed-records.svg';
+import icon3 from '../images/icon-fully-customizable.svg';
 const Features = () => {
   return (
   <fieldset className="bg-neutralGray/10 p-8">
       <Search />
-      <div>
-        <div className="text-center">
+      <div className="flex flex-col gap-10">
+        <div className="text-center flex flex-col gap-2">
           <h3 className="text-3xl font-bold text-neutralViolet">Advanced Statistics</h3>
           <p className="w-1/3 mx-auto text-neutralGrayish">Track how your links are performing across the web with our 
   advanced statistics dashboard.</p>
         </div>
-        <div>
-          <span></span>
+        <div className="flex">
+          <span>
+          </span>
           <span></span>
           <span></span>
         </div>
