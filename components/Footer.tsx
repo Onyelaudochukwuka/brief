41f079bf7fd09d59a57ab3e45e1b29075a0ac3b1
udochukwuka onyela <udochukwukaonyela@gmail.com>
2022-07-03 12:28:13 +0100

    creating navbar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,10 @@ const Footer = () => {
   return (
       <>
       <div className="bg-[url('../images/bg-boost-desktop.svg')]">
-        
+        <h1>Boost your links today</h1>
+        <span>Get Started</span>
       </div> 
+      
       </>
   )
 }
