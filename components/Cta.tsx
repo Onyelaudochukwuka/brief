07b3b04b7d7ce04c9c68b23d1f9c98b98ddb1e1f
udochukwuka onyela <udochukwukaonyela@gmail.com>
2022-07-04 10:56:25 +0100

    finished cta cursor

--- components/Cta.tsx
+++ components/Cta.tsx
@@ -5,7 +5,7 @@ const Cta:NextComponentType = () => {
   return (
       <div className="lg:bg-[url('../images/bg-boost-desktop.svg')] bg-[url('../images/bg-boost-mobile.svg')] lg:bg-[length:100%_120%] lg:bg-bottom bg-[length:100%_150%] bg-center bg-no-repeat w-full bg-primaryDark p-16 text-center text-white flex flex-col items-center gap-6">
           <h1 className="text-xl font-bold">Boost your links today</h1>
-          <span className="p-3 px-8 bg-primaryCol hover:bg-primaryCol/75 w-fit rounded-full">Get Started</span>
+          <span className="p-3 px-8 bg-primaryCol hover:bg-primaryCol/75 w-fit rounded-full cursor-pointer">Get Started</span>
       </div>
   )
 }
