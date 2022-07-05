bd18b9d6ec8cca37647a85fc845e2d4328625fd3
udochukwuka onyela <udochukwukaonyela@gmail.com>
2022-07-05 15:35:34 +0100

    styling

--- components/Result.tsx
+++ components/Result.tsx
@@ -5,12 +5,14 @@ type properties={
 }
 const Result:React.FC<properties> = ({query, result}) => {
   return (
-      <div className="flex gap">
-          <span>{query}</span>
-          <span>
-              <span>{result}</span>
-              <span></span>
-          </span>
+      <div className="flex gap-6 w-3/4 mx-auto">
+          <div className="basis-1/2">
+              <span>{query}</span>
+          </div>
+          <div className="basis-1/2 flex">
+              <span className="basis-1/2">{result}</span>
+              <span className="w-fit p-3 px-5 bg-primaryCol hover:bg-primaryDark transition duration-300 ease-in">Copy</span>
+          </div>
       </div>
   )
 }
