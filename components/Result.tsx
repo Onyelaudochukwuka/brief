69658290fb28bc4bb6f576000310181333aa60e2
udochukwuka onyela <udochukwukaonyela@gmail.com>
2022-07-05 15:50:26 +0100

    fixes

--- components/Result.tsx
+++ components/Result.tsx
@@ -5,13 +5,13 @@ type properties={
 }
 const Result:React.FC<properties> = ({query, result}) => {
   return (
-      <div className="flex gap-6 w-3/4 mx-auto">
+      <div className="flex gap-6 w-3/4 mx-auto p-4 bg-white align-middle items-center">
           <div className="basis-1/2">
               <span>{query}</span>
           </div>
-          <div className="basis-1/2 flex">
-              <span className="basis-1/2">{result}</span>
-              <span className="w-fit p-3 px-5 bg-primaryCol hover:bg-primaryDark transition duration-300 ease-in">Copy</span>
+          <div className="flex gap-6 float-right ml-auto">
+              <span className="basis-1/2 my-auto align-middle text-primaryCol font-bold trac">{result}</span>
+              <span className="w-fit p-2 px-5 bg-primaryCol hover:bg-primaryDark transition duration-300 ease-in cursor-pointer text-white rounded-lg">Copy</span>
           </div>
       </div>
   )
