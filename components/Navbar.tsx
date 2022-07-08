f06012bf754eb6cb66e6b88d4452c30d84c563c3
Udoka <udochukwukaonyela@gmail.com>
2022-07-08 13:06:02 +0100

    created menu icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -36,6 +36,13 @@ const Navbar: NextComponentType = () => {
           </span>
         </div>
       </div>
+      <div className="float-right ml-auto">
+        <div className="w-14 h-1 bg-light rounded-full
+        after:w-14 after:h-1 after:bg-light after:rounded-full after:absolute after:-mt-2.5
+        before:w-14 before:h-1 before:bg-light before:rounded-full before:absolute before:mt-2.5
+                
+        "></div>
+      </div>
     </div>
  ) 
 }
