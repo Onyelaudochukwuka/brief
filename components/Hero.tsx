9aab87cb1d2e39a5e018a124fdd4aa81ffc777b2
Udoka <udochukwukaonyela@gmail.com>
2022-07-08 13:16:39 +0100

    styling hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,23 @@
 import { NextComponentType } from "next"
 import React from 'react';
 import Image from "next/image";
+import Link from "next/link";
+import browserIcon from "../public/browserIcon.svg";
 const Hero:NextComponentType = () => {
   return (
-    <div>
-   
+    <div className="text-light py-16 px-8 flex items-center">
+      <div className="basis-1/2">
+        <div>
+          <h1>Brief Links, Faster Results</h1>
+          <p>An URL Shortener Built With Tools To simplify Things.</p>
+        </div>
+        <div>
+          <span className=""><Link href="/">Get Started</Link></span>
+        </div>
+      </div> 
+      <div className="basis-1/2 p-24">
+        <Image src={browserIcon} layout="responsive" />
+      </div>
     </div>
   )
 }
