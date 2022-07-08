266d9b955002f72de48685205c81fdd121c42145
Udoka <udochukwukaonyela@gmail.com>
2022-07-08 11:30:53 +0100

    deleted useless icons

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import Search from "./Search"
 import Image from "next/image"
-import icon1 from '../images/icon-brand-recognition.svg';
-import icon2 from '../images/icon-detailed-records.svg';
-import icon3 from '../images/icon-fully-customizable.svg';
 import { NextComponentType } from "next";
 import Result from "./Result";
 const Features:React.FC = () => {
