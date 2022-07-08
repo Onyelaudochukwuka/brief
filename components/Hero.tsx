9decec14be7ea2a6f44f56b007dd2a5d8669c264
Udoka <udochukwukaonyela@gmail.com>
2022-07-08 13:40:34 +0100

    background and display styled

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,13 +9,13 @@ const Hero:NextComponentType = () => {
       <div className="basis-1/2 flex items-baseline flex-col">
         <div className="basis-1/2">
           <h1 className="text-[44px] font-bold">Brief Links, Faster Results</h1>
-          <p className="font-semibold text-[28px]">An URL Shortener Built With Tools To simplify Things.</p>
+          <p className="font-semibold text-[28px] leading-relaxed">An URL Shortener Built With Tools To simplify Things.</p>
         </div>
-        <div className="basis-1/2">
-          <span className="p-8 bg-dark"><Link href="/">Get Started</Link></span>
+        <div className="basis-1/2 mt-16">
+          <span className="p-6 rounded-xl shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] bg-dark font-bold tracking-wide hover:bg-neutral hover:shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)]"><Link href="/">Get Started</Link></span>
         </div>
       </div> 
-      <div className="basis-2/5">
+      <div className="basis-2/5 float-right ml-auto my-auto">
         {/* <div className="w-96 h-fit"> */}
         <Image src={browserIcon} layout="responsive" />
       {/* </div> */}
