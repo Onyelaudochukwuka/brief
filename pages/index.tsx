b80da563b43dd35fbaabf6af3150c3f1d410883c
Udoka <udochukwukaonyela@gmail.com>
2022-07-08 14:00:22 +0100

    Importen Shorten Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import {Features, Hero} from "../components";
+import { Features, Hero, Shorten } from "../components";
 
 const Home: NextPage = () => {
   return (
     <>
     <Hero />
-     <Features />
+      <Features />
+      <Shorten />
     </>
   )
 }
