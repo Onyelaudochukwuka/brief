2bcedefc3d1b4c2e51b6692384c3ce8486777bde
Udoka <udochukwukaonyela@gmail.com>
2022-07-08 13:50:49 +0100

    created hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero:NextComponentType = () => {
           <p className="font-semibold text-[28px] leading-relaxed">An URL Shortener Built With Tools To simplify Things.</p>
         </div>
         <div className="basis-1/2 mt-16">
-          <span className="p-6 rounded-xl shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] bg-dark font-bold tracking-wide hover:bg-neutral hover:shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)]"><Link href="/">Get Started</Link></span>
+          <span className="p-6 rounded-xl shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] bg-dark font-bold tracking-wide hover:bg-neutral hover:shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)] transition-all duration-500 ease-in-out cursor-pointer"><Link href="/">Get Started</Link></span>
         </div>
       </div> 
       <div className="basis-2/5 float-right ml-auto my-auto">
