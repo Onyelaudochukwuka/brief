58e415c00d2e403ee90ed75773599681d2279970
Udoka <udochukwukaonyela@gmail.com>
2022-07-08 11:44:05 +0100

    Adding Links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Link from "next/link";
 import { NextComponentType } from "next";
 import logo from '../public/icons/Brief.svg';
+import Image from "next/image";
 const Navbar: NextComponentType = () => {
   const [drop, setDrop] = useState(true);
   useEffect(() => {
@@ -9,7 +10,32 @@ const Navbar: NextComponentType = () => {
   },[drop])
   return (
     <div>
-      
+      <div><Image src={logo} layout="responsive" />
+      <div>
+        <div>
+          <span>
+            <Link href="/"></Link>
+          </span>
+          <span>
+            <Link href="/"></Link>
+          </span>
+          <span>
+            <Link href="/"></Link>
+          </span>
+          <span>
+            <Link href="/"></Link>
+          </span>
+        </div>
+        <div>
+          <span>
+            <Link href="/"></Link>
+          </span>
+          <span>
+            <Link href="/"></Link>
+          </span>
+        </div>
+      </div>
+    </div>
     </div>
  ) 
 }
