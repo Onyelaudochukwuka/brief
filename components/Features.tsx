779aa2291899a9ec92bc23f51a1ac995ea023af2
Udoka <udochukwukaonyela@gmail.com>
2022-07-08 17:47:21 +0100

    added data array

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,39 +1,36 @@
 import React from 'react'
 import Image from "next/image"
 import { NextComponentType } from "next";
-import Result from "./Result";
-const Features:NextComponentType = () => {
+import brand from '../public/monstersIllustration.svg';
+import detailed from '../public/copyIllustration.svg';
+import customizable from '../public/fullCustomizable.svg';
+
+const details = [
+  {
+  icon: brand,
+  heading: "Brand Recognition",
+  details: "Boost Your Brand Recognition with Every Click, Branded Links help instil confident in your content."
+  },
+  {
+    icon: detailed,
+    heading: "Detailed Records",
+    details: "Gain insight into when your links are clicked. And booster your brands potential with that data.",
+  },
+  {
+    icon: customizable,
+    heading: "Fully Customizable",
+    details: "Improve brand awareness and content discoverability through customizable links, supercharging audience"
+  }
+]
+const Features: NextComponentType = () => {
   return (
-    <div className="py-16">
+    <div className="py-16 text-light">
       <span>
-        <h2></h2>
-        <h4></h4>
+        <h2>The Link Shortener You Can Trust</h2>
+        <h4>Add more credibility to your brand with a link everyone loves to click</h4>
       </span>
       <div>
-        <div>
-          <span>
-            <div>
-              <h4></h4>
-              <p></p>
-            </div>
-          </span>
-        </div> 
-        <div>
-          <span>
-            <div>
-              <h4></h4>
-              <p></p>
-            </div>
-          </span>
-        </div>
-        <div>
-          <span>
-            <div>
-              <h4></h4>
-              <p></p>
-            </div>
-          </span>
-        </div>
+       
       </div>
   </div>
   )
