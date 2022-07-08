2128ff99596cc56bea3b1e759ed56deb272ced57
Udoka <udochukwukaonyela@gmail.com>
2022-07-08 22:28:58 +0100

    component export

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export { default as Hero } from "./Hero";
 export { default as Features } from "./Features";
 export { default as Shorten } from "./Shorten";
+export { default as Cta } from "./Cta";
