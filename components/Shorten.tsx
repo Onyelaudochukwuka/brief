9d408d9fd24f93545b4db3fb33314f4f3e18ce10
Udoka <udochukwukaonyela@gmail.com>
2022-07-08 14:06:37 +0100

    Styling Component

--- components/Shorten.tsx
+++ components/Shorten.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Shorten:NextComponentType = () => {
   return (
-    <div className="linear p-12">
-      <input type="text" placeholder="https://brief.dev/E8bCl293" />
-      <button type="submit">Shorten</button>
+    <div className="linear w-3/4 m-auto p-12 px-24 flex items-center">
+      <input type="text" placeholder="https://brief.dev/E8bCl293" className="basis-3/4 bg-"/>
+      <button type="submit" className="w-1/4">Shorten</button>
     </div>
   )
 }
