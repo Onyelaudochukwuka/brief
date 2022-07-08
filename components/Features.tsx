df5340a4c261ff75ad8bcba5f1649c5ef3e5a303
Udoka <udochukwukaonyela@gmail.com>
2022-07-08 17:04:27 +0100

    code boilerplate

--- components/Features.tsx
+++ components/Features.tsx
@@ -2,10 +2,39 @@ import React from 'react'
 import Image from "next/image"
 import { NextComponentType } from "next";
 import Result from "./Result";
-const Features:React.FC = () => {
+const Features:NextComponentType = () => {
   return (
     <div className="py-16">
-      
+      <span>
+        <h2></h2>
+        <h4></h4>
+      </span>
+      <div>
+        <div>
+          <span>
+            <div>
+              <h4></h4>
+              <p></p>
+            </div>
+          </span>
+        </div> 
+        <div>
+          <span>
+            <div>
+              <h4></h4>
+              <p></p>
+            </div>
+          </span>
+        </div>
+        <div>
+          <span>
+            <div>
+              <h4></h4>
+              <p></p>
+            </div>
+          </span>
+        </div>
+      </div>
   </div>
   )
 }
