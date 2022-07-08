7154ee350936568c1397f6e3a249558004be719f
Udoka <udochukwukaonyela@gmail.com>
2022-07-08 22:55:51 +0100

    created data array

--- components/Cta.tsx
+++ components/Cta.tsx
@@ -2,16 +2,32 @@ import { NextComponentType } from "next"
 import Image from "next/image";
 import React from 'react';
 import icon from '../public/icons/arrow.svg';
+const data = [
+  {
+    header: "What is an URL shortener",
+    details: "URL Shortener is a kind of a tool that allows you to shorten the URL, which is often in the form of: “https”http”. Using this tool, you will be able to easily access your website by entering just one key, and you will be automatically redirected to your website."
+  },
+  {
+    header: "Benefit of a Short Url",
+    details: "Shorter URLs Make Sharing Easier and also giving a short link which is more memorable means users can comeback to your site without needing to search through their history tab for hour's."
+  },
+  {
+    header: "What is a Custom Url Shortener",
+    details: "Why choose brief"
+  },
+  {
+    header: "Why choose brief",
+    details: ""
+  }
+]
 const Cta:NextComponentType = () => {
   return (
-    <div>
+    <div className="p-4">
       <div>
         <h1></h1>
         <div>
-          <div><h4>What Is an URL Shortener</h4><span className="block"><Image src={icon} layout="responsive" /></span></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          {data.map(({ header, details }) => <div className="flex w-2/5 border-b border-light mx-auto justify-between text-light p-2 items-center"><h4>{header}</h4><span className="block w-8 h-8"><Image src={icon} layout="responsive" /></span></div>)}
+          
         </div>
       </div>
     </div>
