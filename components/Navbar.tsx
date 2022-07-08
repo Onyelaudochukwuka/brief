067e008ba0ff9421ad91e748350433be55747f1d
Udoka <udochukwukaonyela@gmail.com>
2022-07-08 12:21:42 +0100

    stylying links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Link from "next/link";
 import { NextComponentType } from "next";
-import logo from '../public/icons/Brief.svg';
+import logo from '../public/Brief.svg';
 import Image from "next/image";
 const Navbar: NextComponentType = () => {
   const [drop, setDrop] = useState(true);
@@ -9,33 +9,33 @@ const Navbar: NextComponentType = () => {
     console.log(drop);
   },[drop])
   return (
-    <div>
-      <div><Image src={logo} layout="responsive" />
-      <div>
-        <div>
-          <span>
-            <Link href="/"></Link>
+    <div className="flex w-full p-6 items-center text-light bg-dark">
+      <div className="basis-2/5">
+      <div className="w-24 h-fit">
+        <Image src={logo} layout="responsive" />
+        </div>
+        </div>
+      <div className="flex basis-3/5 w-full">
+        <div className="flex w-full basis-3/5">
+          <span className="basis-1/3">
+            <Link href="/">Why briefly</Link>
           </span>
-          <span>
-            <Link href="/"></Link>
+          <span className="basis-1/3">
+            <Link href="/">Products</Link>
           </span>
-          <span>
-            <Link href="/"></Link>
-          </span>
-          <span>
-            <Link href="/"></Link>
+          <span className="basis-1/3">
+            <Link href="/">Resources</Link>
           </span>
         </div>
-        <div>
-          <span>
-            <Link href="/"></Link>
+          <div className="flex basis-2/5 gap-6">
+          <span className="">
+            <Link href="/">Login</Link>
           </span>
-          <span>
-            <Link href="/"></Link>
+          <span className="">
+            <Link href="/">Sign Up</Link>
           </span>
         </div>
       </div>
-    </div>
     </div>
  ) 
 }
