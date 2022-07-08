1e455221f8f40f8e7637837ea81f8cfb9f97e791
Udoka <udochukwukaonyela@gmail.com>
2022-07-08 13:27:57 +0100

    fixes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,18 +5,20 @@ import Link from "next/link";
 import browserIcon from "../public/browserIcon.svg";
 const Hero:NextComponentType = () => {
   return (
-    <div className="text-light py-16 px-8 flex items-center">
-      <div className="basis-1/2">
-        <div>
-          <h1>Brief Links, Faster Results</h1>
-          <p>An URL Shortener Built With Tools To simplify Things.</p>
+    <div className="text-light py-24 px-12 flex items-center">
+      <div className="basis-1/2 flex items-baseline flex-col">
+        <div className="basis-1/2">
+          <h1 className="text-[44px] font-bold">Brief Links, Faster Results</h1>
+          <p className="font-semibold text-[28px]">An URL Shortener Built With Tools To simplify Things.</p>
         </div>
-        <div>
-          <span className=""><Link href="/">Get Started</Link></span>
+        <div className="basis-1/2">
+          <span className="p-8 bg-dark"><Link href="/">Get Started</Link></span>
         </div>
       </div> 
-      <div className="basis-1/2 p-24">
+      <div className="basis-2/5">
+        {/* <div className="w-96 h-fit"> */}
         <Image src={browserIcon} layout="responsive" />
+      {/* </div> */}
       </div>
     </div>
   )
