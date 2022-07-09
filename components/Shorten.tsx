cd10b5c499985eafe5b7608e688b87ca20ed8669
Udoka <udochukwukaonyela@gmail.com>
2022-07-09 09:17:06 +0100

    mobile responsive ness

--- components/Shorten.tsx
+++ components/Shorten.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Shorten:NextComponentType = () => {
   return (
-    <div className="linear w-3/4 m-auto p-12 px-24 flex items-center gap-6">
-      <input type="text" placeholder="https://brief.dev/E8bCl293" className="basis-3/4 bg-transparent border-b-2 border-b-neutral focus:border-0 text-lg"/>
+    <div className="linear w-3/4 m-auto p-12 px-24 flex lg:flex-row flex-col items-center gap-6">
+      <input type="text" placeholder="https://brief.dev/E8bCl293" className="basis-3/4 bg-transparent border-b-2 border-b-neutral focus:border-0 focus:border-b-2 focus:ring-0 text-lg text-light"/>
       <button type="submit" className="px-6 py-3 text-lg outline-0 focus:outline-0 focus:border-0 bg-neutral text-light rounded-lg font-bold">Shorten</button>
     </div>
   )
