1e9823e86d75b27b7424f53405cbe060917d35c2
Udoka <udochukwukaonyela@gmail.com>
2022-07-09 08:21:10 +0100

    mobile responsiveness

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,17 +5,17 @@ import Link from "next/link";
 import browserIcon from "../public/browserIcon.svg";
 const Hero:NextComponentType = () => {
   return (
-    <div className="text-light py-24 px-12 flex items-center">
-      <div className="basis-1/2 flex items-baseline flex-col">
+    <div className="text-light lg:py-24 py-12 lg:px-12 flex px-8 lg:items-center lg:flex-row flex-col-reverse">
+      <div className="lg:basis-1/2 flex lg:items-baseline flex-col">
         <div className="basis-1/2">
-          <h1 className="text-[44px] font-bold">Brief Links, Faster Results</h1>
-          <p className="font-semibold text-[28px] leading-relaxed">An URL Shortener Built With Tools To simplify Things.</p>
+          <h1 className="lg:text-[44px] text-2xl font-bold">Brief Links, Faster Results</h1>
+          <p className="font-semibold lg:text-[28px] text-base leading-relaxed">An URL Shortener Built With Tools To simplify Things.</p>
         </div>
         <div className="basis-1/2 mt-16">
-          <span className="p-6 rounded-xl shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] bg-dark font-bold tracking-wide hover:bg-neutral hover:shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)] transition-all duration-500 ease-in-out cursor-pointer"><Link href="/">Get Started</Link></span>
+          <span className="lg:p-6  p-4 rounded-xl shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] bg-dark font-bold tracking-wide hover:bg-neutral hover:shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)] transition-all duration-500 ease-in-out cursor-pointer"><Link href="/">Get Started</Link></span>
         </div>
       </div> 
-      <div className="basis-2/5 float-right ml-auto my-auto">
+      <div className="lg:basis-2/5 lg:float-right lg:ml-auto lg:my-auto w-3/5">
         {/* <div className="w-96 h-fit"> */}
         <Image src={browserIcon} layout="responsive" />
       {/* </div> */}
