351011e3902513ca2dd6164b7a715903908a4435
Udoka <udochukwukaonyela@gmail.com>
2022-07-09 10:15:09 +0100

    header icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import '../public/Brief.ico';
 import { Cta, Features, Hero, Shorten } from "../components";
-
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>Brief</title>
-        <link rel="shortcut icon" href="../public/Brief.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="../public/brief.svg"/>
       </Head>
     <Hero />
       <Shorten />
