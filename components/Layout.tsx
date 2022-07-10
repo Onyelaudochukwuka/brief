60cd27372fcb6e9ac9437f2ff1b0787bacde8887
Udoka <udochukwukaonyela@gmail.com>
2022-07-10 15:28:00 +0100

    added icon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,9 +4,14 @@ import Navbar from "./Navbar"
 import Footer from "./Footer"
 import { ScriptProps } from "next/script"
 import { NextComponentType } from "next"
+import Head from "next/head"
 const Layout = ({children}:any) => {
   return (
     <div className="bg-darkPrimary">
+      <Head>
+        <title>Brief</title>
+        <link rel="shortcut icon" href="/brief.ico" />
+      </Head>
       <Navbar />
       {children}
       <Footer />
