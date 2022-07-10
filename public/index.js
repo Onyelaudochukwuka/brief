e87afdeebb50ff123002e849b63ad69add5b5bf6
Udoka <udochukwukaonyela@gmail.com>
2022-07-10 14:11:51 +0100

    exported all icons

---
+++ public/index.js
@@ -0,0 +1,12 @@
+import brief from './brief.svg';
+import browserIcon from './browserIcon.svg';
+import copyIllustration from './copyIllustration.svg';
+import monstersIllustration from 'monstersIllustration.svg';
+import fullyCustomizable from 'fullyCustomizable.svg';
+import arrow from 'arrow.svg';
+import facebook from 'facebook.svg';
+import twitter from 'twitter.svg';
+import instagram from 'instagram.svg';
+
+
+export { brief, browserIcon, copyIllustration, monstersIllustration, fullyCustomizable, arrow, facebook, twitter, instagram };
