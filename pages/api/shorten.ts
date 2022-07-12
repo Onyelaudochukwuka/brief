87743b5045538830ae2e3efe7cf9fcad41d127e4
Udoka <udochukwukaonyela@gmail.com>
2022-07-12 21:28:55 +0100

    initialized requests

--- pages/api/shorten.ts
+++ pages/api/shorten.ts
@@ -1,28 +1,47 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import dbConnect from '../../lib/dbConnect';
-import User from '../../models/Url';
+const Url = require('../../models/Url');
+const validUrl = require('valid-url');
+const shortid = require('shortid');
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-export default async function handler (req:NextApiRequest, res:NextApiResponse) {
-  const { method } = req
-
+  const { method } = req;
+  const { longUrl } = req.body;
+  const baseUrl = process.env.BASE_URL;
   await dbConnect()
 
   switch (method) {
-    case 'GET':
-      try {    
-        const users = await User.find({})
-        res.status(200).json({ success: true, data: users })
-      } catch (error) {
-        res.status(400).json({ success: false })
-      }
-      break
     case 'POST':
-      try {
-        const user = await User.create(req.body)
-        res.status(201).json({ success: true, data: user })
-      } catch (error) {
-        res.status(400).json({ success: false })
+        if (!validUrl.isUri(baseUrl)) {
+        return res.status(401).json('invalid base url');
+    }
+      const urlCode = shortid.generate();
+      if (validUrl.isUri(longUrl)) {
+        try {
+          let url = await Url.findOne({ longUrl });
+          if (url) {
+            res.json(url);
+          } else {
+            const shortUrl = baseUrl + '/' + urlCode;
+
+            url = new Url({
+              longUrl,
+              shortUrl,
+              urlCode,
+              date: new Date()
+            });
+            await url.save();
+
+            res.json(url);
+          }
+        } catch (err) {
+          console.error(err);
+          res.status(500).json('Server error');
+        }
       }
+         else {
+        res.status(401).json('Invalid long url');
+    }
       break
     default:
       res.status(400).json({ success: false })
