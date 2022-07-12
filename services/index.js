c53f78aeaf192fc9a94db8b23fbc6e6ffe9fb853
Udoka <udochukwukaonyela@gmail.com>
2022-07-12 22:40:39 +0100

    connected api

---
+++ services/index.js
@@ -0,0 +1,10 @@
+export const shortenLink = async (obj) => {
+    const result = await fetch('/api/shorten', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(obj),
+    });
+    return result.json();
+}
