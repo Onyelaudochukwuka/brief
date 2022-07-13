7da3a5ec85523e10c2101a8479073fba72d9d9b2
Udoka <udochukwukaonyela@gmail.com>
2022-07-13 11:10:49 +0100

    styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,8 +3,17 @@ import Link from "next/link";
 import { NextComponentType } from "next";
 import { brief as logo} from '../public';
 import Image from "next/image";
+import { useRouter } from 'next/router';
+import { useSession, signIn, signOut } from "next-auth/react";
 const Navbar: NextComponentType = () => {
   const [drop, setDrop] = useState(true);
+  const { data: session } = useSession();
+  const { push, asPath } = useRouter();
+  console.log(session);
+  const handleSignOut = async () => {
+    const data = await signOut({ redirect: false, callbackUrl: "/" });
+    push(data.url);
+  };
   useEffect(() => {
     console.log(drop);
   },[drop])
@@ -15,7 +24,13 @@ const Navbar: NextComponentType = () => {
         <Image src={logo} layout="responsive" />
         </div>
         </div>
-      <div className={`hidden lg:flex basis-3/5 w-full items-center`}>
+      {session ?
+        <div className=" flex items-center justify-center gap-8 float-right ml-auto">
+          <span>{session.user?.name}</span>
+          <img src={session.user?.image || ""} className="w-10 h-auto rounded-full" />
+        </div>
+        :
+     <div className={`hidden lg:flex basis-3/5 w-full items-center`}>
         <div className="flex w-full basis-3/5">
           <span className="basis-1/3 hover:text-neutral transition-all duration-500 ease-in-out">
             <Link href="/">Why briefly</Link>
@@ -28,14 +43,16 @@ const Navbar: NextComponentType = () => {
           </span>
         </div>
           <div className="flex basis-2/5 gap-6 items-center ml-6">
-          <span className=" hover:text-neutral transition-all duration-500 ease-in-out">
-            <Link href="/">Login</Link>
+          <span className=" hover:text-neutral transition-all duration-500 ease-in-out" onClick={()=>signIn()}>
+            Login
           </span>
           <span className="bg-neutral py-3 px-6 rounded-full shadow-[0_4px_4px_1px_rgba(4,92,148,0.45)] active:scale-95 hover:bg-[#4097CE] hover:shadow-[0_5px_4px_0px_rgba(64,151,206,0.75)] active:shadow-[0_6px_4px_1px_rgba(64,151,206,0.75)] cursor-pointer transition-all duration-500 ease-in-out">
             <Link href="/">Sign Up</Link>
           </span>
         </div>
       </div>
+     }
+        
       <div className="lg:hidden float-right ml-auto">
         <div className="w-14 h-1 bg-light rounded-full cursor-pointer
         after:w-14 after:h-1 after:bg-light after:rounded-full after:absolute after:-mt-2.5 after:cursor-pointer
