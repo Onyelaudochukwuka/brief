83ded1b28530ed947b13abd4972ea5ae17c1d9a0
Udoka <udochukwukaonyela@gmail.com>
2022-07-14 18:34:10 +0100

    created navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Link from "next/link";
 import { NextComponentType } from "next";
 import { brief as logo} from '../public';
@@ -14,39 +14,34 @@ const Navbar: NextComponentType = () => {
     const data = await signOut({ redirect: false, callbackUrl: "/" });
     push(data.url);
   };
-  useEffect(() => {
-    console.log(drop);
-  },[drop])
   return (
     <div className="flex w-full p-6 items-center text-light bg-navBar font-semibold">
       <div className="basis-2/5">
-      <div className="w-24 h-fit">
-        <Image src={logo} layout="responsive" />
+        <div className="w-24 h-fit">
+          <Image src={logo} layout="responsive" />
         </div>
-        </div>
-      {session ?
-        <div className=" flex items-center justify-center gap-8 float-right ml-auto">
-          <span onClick={()=>handleSignOut()}>{session.user?.name}</span>
-          <img src={session.user?.image || ""} className="w-10 h-auto rounded-full" />
-        </div>
-        :
-     <div className={`hidden lg:flex basis-3/5 w-full items-center`}>
+      </div>
+      <div className={`hidden lg:flex basis-3/5 w-full items-center`}>
         <div className="flex w-full basis-3/5">
           <span className="basis-1/3 hover:text-neutral transition-all duration-500 ease-in-out">
-            <Link href="/products">Products</Link>
+            <Link href="/">Why briefly</Link>
+          </span>
+          <span className="basis-1/3 hover:text-neutral transition-all duration-500 ease-in-out">
+            <Link href="/">Products</Link>
           </span>
           <span className="basis-1/3 hover:text-neutral transition-all duration-500 ease-in-out">
             <Link href="/">Resources</Link>
           </span>
         </div>
-          <div className="flex basis-2/5 gap-6 items-center ml-6">
-            <span className="bg-neutral py-3 px-6 rounded-full shadow-[0_4px_4px_1px_rgba(4,92,148,0.45)] active:scale-95 hover:bg-[#4097CE] hover:shadow-[0_5px_4px_0px_rgba(64,151,206,0.75)] active:shadow-[0_6px_4px_1px_rgba(64,151,206,0.75)] cursor-pointer transition-all duration-500 ease-in" onClick={() => signIn()}>
-                LogIn
+        <div className="flex basis-2/5 gap-6 items-center ml-6">
+          <span className=" hover:text-neutral transition-all duration-500 ease-in-out">
+            <Link href="/">Login</Link>
+          </span>
+          <span className="bg-neutral py-3 px-6 rounded-full shadow-[0_4px_4px_1px_rgba(4,92,148,0.45)] active:scale-95 hover:bg-[#4097CE] hover:shadow-[0_5px_4px_0px_rgba(64,151,206,0.75)] active:shadow-[0_6px_4px_1px_rgba(64,151,206,0.75)] cursor-pointer transition-all duration-500 ease-in-out">
+            <Link href="/">Sign Up</Link>
           </span>
         </div>
       </div>
-     }
-        
       <div className="lg:hidden float-right ml-auto">
         <div className="w-14 h-1 bg-light rounded-full cursor-pointer
         after:w-14 after:h-1 after:bg-light after:rounded-full after:absolute after:-mt-2.5 after:cursor-pointer
