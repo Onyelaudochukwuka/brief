372950c6ef2b436c42fe5514728d872ccf192a39
Udoka <udochukwukaonyela@gmail.com>
2022-07-14 18:44:29 +0100

    adde components

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import '../public/Brief.ico';
 import { Cta, Features, Hero, Shorten } from "../components";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 const Home: NextPage = () => {
   return (
     <>
+    <Navbar />
     <Hero />
       <Shorten />
       <Features />
       <Cta />
+      <Footer />
     </>
   )
 }
