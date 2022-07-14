3b200135e4982c8311cebb7af2e882ae261a5498
Udoka <udochukwukaonyela@gmail.com>
2022-07-14 20:32:51 +0100

    added pages

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -19,5 +19,8 @@ export default NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
+    pages: {
+        signIn: '/auth/signIn'
+    },
     adapter: MongoDBAdapter(clientPromise),
 });
