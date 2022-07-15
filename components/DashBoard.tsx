2f7fc3b1a9364f0f004b6fe3c36b1ef10ad85902
Udoka <udochukwukaonyela@gmail.com>
2022-07-15 15:14:19 +0100

    styling

--- components/DashBoard.tsx
+++ components/DashBoard.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import {NextComponentType} from 'next'
+import React, { useState } from 'react'
+import { NextComponentType } from 'next';
+import { useSession } from "next-auth/react";
 type Props = {}
-const DashBoard:NextComponentType = (props: Props) => {
+const DashBoard: NextComponentType = ({}: Props) => {
+  const { data: session } = useSession();
+  const [check, setCheck] = useState(false)
   return (
     <div className="flex p-6 bg-navBar">
-        
+      <div className="w-16 bg-lightNeutral rounded-full h-6">
+<div className="w-3 h-3 rounded-full bg-dark"></div>
+      </div> 
     </div>
   )
 }
