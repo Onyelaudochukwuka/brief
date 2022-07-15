d9ae3e0982ff5b30be96afaa3d28969e81a60d95
Udoka <udochukwukaonyela@gmail.com>
2022-07-15 21:37:14 +0100

    exported Analytics dashboard

--- components/index.js
+++ components/index.js
@@ -3,4 +3,4 @@ export { default as Features } from "./Features";
 export { default as Shorten } from "./Shorten";
 export { default as Cta } from "./Cta";
 export { default as DashBoard } from './DashBoard';
-export { default as LinkShortener } from './LinkShortener';
+export { default as AnalyticsDashboard } from './AnalyticsDashboard';
