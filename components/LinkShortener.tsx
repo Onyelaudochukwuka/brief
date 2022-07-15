7a4eb2e3615e70818fcdc7484e50cbec07a2640d
Udoka <udochukwukaonyela@gmail.com>
2022-07-15 18:12:46 +0100

    created component for passing link data

---
+++ components/LinkShortener.tsx
@@ -0,0 +1,12 @@
+import { NextComponentType } from "next"
+import React from 'react'
+
+type Props = {}
+
+const LinkShortener:NextComponentType = ({}: Props) => {
+  return (
+    <div>LinkShortener</div>
+  )
+}
+
+export default LinkShortener;
