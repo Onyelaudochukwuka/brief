24cc2f15e3ae7591ba25a35109e4db20dd7f2120
Udoka <udochukwukaonyela@gmail.com>
2022-07-15 12:00:09 +0100

    exported dashboard

--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export { default as Hero } from "./Hero";
 export { default as Features } from "./Features";
 export { default as Shorten } from "./Shorten";
 export { default as Cta } from "./Cta";
+export { default as DashBoard } from './DashBoard'
