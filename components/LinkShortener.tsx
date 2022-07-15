5ac8820fba065758400f7c16b2b252994e2bcc09
Udoka <udochukwukaonyela@gmail.com>
2022-07-15 21:24:01 +0100

    styling

--- components/LinkShortener.tsx
+++ components/LinkShortener.tsx
@@ -1,15 +1,22 @@
 import { NextComponentType } from "next"
 import Image from "next/image"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { link } from "../public"
 
 type Props = {
     check?: boolean;
 }
 
+
 const LinkShortener = ({check}: Props) => {
     const [linkVal, setLinkVal] = useState('');
-    const [displayCustom, setDisplayCustom] = useState(false)
+    const [displayCustom, setDisplayCustom] = useState(false);
+    useEffect(() => {
+        if (check) {
+            return setDisplayCustom(false);
+        }
+    }, [check])
+    
   return (
       <div className="flex items-center justify-center mt-12 gap-24 flex-col">
           <h1 className="text-light text-xl font-bold tracking-widest">Shorten Link</h1>
@@ -38,9 +45,9 @@ const LinkShortener = ({check}: Props) => {
                   if (!check) return setDisplayCustom(true);
               }} >
               <div className="lg:w-1/2 w-4/5 flex items-center justify-center mx-auto mt-8 gap-6">
-                  <span className="transition-all duration-500 ease-in bg-[transparent] w-full py-4 pl-20 pr-3 shadow-sm outline-none ring-2 focus:ring-neutral ring-dark ring-offset-dark ring-offset-1 focus:ring-offset-neutral/30 h-[auto] align-middle my-auto rounded-md text-light placeholder:text-light placeholder:text-lg text-lg placeholder:font-bold font-bold flex items-center justify-center"><label htmlFor="custom">https://brief.site/api/</label><input type={"text"} id="custom" className="bg-[transparent] outline-none border-b-2 focus:border-neutral border-dark h-[auto] align-middle my-auto rounded-sm text-light placeholder:text-light/ placeholder:text-lg text-lg placeholder:font-bold font-bold w-fit" placeholder="Extension"/></span>
+                  <span className="transition-all duration-500 ease-in bg-[transparent] w-full py-4 pl-20 pr-3 shadow-sm outline-none ring-2 focus:ring-neutral ring-dark ring-offset-dark ring-offset-1 focus:ring-offset-neutral/30 h-[auto] align-middle my-auto rounded-md text-light placeholder:text-light placeholder:text-lg text-lg placeholder:font-bold font-bold flex items-center justify-center"><label htmlFor="custom">https://brief.site/api/</label><input type={"text"} id="custom" className="bg-[transparent] outline-none border-b-2 focus:border-neutral border-dark h-[auto] align-middle my-auto rounded-sm text-light placeholder:text-light/50 placeholder:text-lg text-lg placeholder:font-bold font-bold w-fit" placeholder="Extension"/></span>
 
-                  <button className="p-4  transition-all duration-500 ease-in font-semibold rounded cursor-pointer" type="submit">Shorten</button>
+                  <button className="p-4 font-semibold rounded  bg-neutral text-light w-fit hover:shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] tracking-wide hover:bg-dark shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)]  transition-[box-shadow,background,color] duration-500 ease-in-out cursor-pointer active:scale-90" type="submit">Shorten</button>
               </div>
               </form>
           }
