0cab41db9beb41c3d9abead4b948d1c99962187a
Udoka <udochukwukaonyela@gmail.com>
2022-07-15 11:52:33 +0100

    styling component

---
+++ components/DashBoard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {NextComponentType} from 'next'
+type Props = {}
+const DashBoard:NextComponentType = (props: Props) => {
+  return (
+    <div className>
+        
+    </div>
+  )
+}
+
+export default DashBoard
