75a73ad7ceccbecf6acb1a86b4f0b4ad74e35556
Udoka <udochukwukaonyela@gmail.com>
2022-07-15 15:57:22 +0100

    styling

--- components/DashBoard.tsx
+++ components/DashBoard.tsx
@@ -7,7 +7,7 @@ const DashBoard: NextComponentType = ({}: Props) => {
   const [check, setCheck] = useState(false)
   return (
     <div className="flex p-6 bg-navBar">
-      <div className="w-32 bg-lightNeutral rounded-full h-10 flex items-center justify-center">
+      <div className={`w-max bg-${check ? `neutral` : `lightNeutral`} rounded-full h-10 flex items-center justify-center transition duration-500 gap-1 px-1`}>
         <div className={`w-14 h-10 rounded-full bg-dark cursor-pointer transition-[order] duration-300 order-${check ? `1` : `2`}`} onClick={()=>setCheck(!check)}></div>
         <span className="text-lg font-bold transition-all duration-500">{check ? "Default" : "Custom"}</span>
       </div> 
