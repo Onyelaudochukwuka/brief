9d048a2804aa0608a2886b598203620e871d2cf0
Udoka <udochukwukaonyela@gmail.com>
2022-07-16 09:14:42 +0100

    email login

--- pages/auth/signIn.tsx
+++ pages/auth/signIn.tsx
@@ -48,7 +48,7 @@ const signIn = () => {
         <form onSubmit={(e) => {
           e.preventDefault();
           if (!email) return false;
-          return SignIn('email', { email, redirect: false })
+          return SignIn('email', { email, redirect: false }).then(()=>console.log("then"))
         }} className="flex flex-col w-5/12 gap-6">
           <input type={"email"} value={email} onChange={(e) => setEmail(e.target.value)} className="outline-none p-4 pl-6 ring-neutral rounded-sm text-neutral ring-2 focus:ring-dark placeholder:text-neutral w-full" placeholder="Email" />
           <button type={"submit"} className="w-full p-4 text-center font-bold text-lg bg-neutral hover:text-light/80 text-light rounded-md hover:bg-dark cursor-pointer transition-[box-shadow,background,color,transform] duration-500 ease-in-out hover:shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] tracking-wide shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)] active:scale-95"> LogIn </button>
