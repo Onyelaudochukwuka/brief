2232a31ba9004872ae62c37ad191c3898b3dcdb7
Udoka <udochukwukaonyela@gmail.com>
2022-07-16 20:44:53 +0100

    created routes

--- pages/api/analytics.ts
+++ pages/api/analytics.ts
@@ -5,46 +5,25 @@ const validUrl = require('valid-url');
 const shortid = require('shortid');
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
-  const { longUrl, customExt, email } = req.body;
-  const baseUrl = process.env.BASE_URL;
+  const {  email } = req.body;
   await dbConnect()
 
   switch (method) {
-    case 'POST':
-        if (!validUrl.isUri(baseUrl)) {
-        return res.status(401).json('invalid base url');
-    }
-      const Code = shortid.generate();
-      const urlCode = customExt ? customExt : Code;
-      if (validUrl.isUri(longUrl)) {
+    case 'GET':
         try {
-          let url = await Url.findOne({ longUrl, userEmail: email });
+          let url = await Url.find({  userEmail: {$eq: email} });
             if (url) {
-            res.json(url);
+            res.status(200).json(url);
           } else {
-            const shortUrl = baseUrl + urlCode;
-                url = new Url({
-                    userEmail: email,
-                    longUrl,
-                    shortUrl,
-                    urlCode,
-                    date: new Date()
-                });
-            await url.save();
-
-            res.json(url);
+            res.status(200).json({});
           }
         } catch (err) {
           console.error(err);
           res.status(500).json('Server error');
         }
-      }
-         else {
-        res.status(401).json('Invalid long url');
-    }
       break
     default:
-      res.status(400).json({ success: false })
+    res.status(400).json({ success: false })
       break
   }
 }
