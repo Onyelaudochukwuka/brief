07693c2e802d121e52a12ef0b64e029b65e7605a
Udoka <udochukwukaonyela@gmail.com>
2022-07-16 23:21:59 +0100

    creating loader

---
+++ components/Loader.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Loader = (props: Props) => {
+  return (
+      <div className="border-3 ">
+          
+    </div>
+  )
+}
+
+export default Loader
