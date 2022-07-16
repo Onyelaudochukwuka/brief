7845c920b91bb7d8d66525a2021e9b1c52b21b7a
Udoka <udochukwukaonyela@gmail.com>
2022-07-16 21:24:13 +0100

    Created a result component to display the result of the query

---
+++ components/LinkResult.tsx
@@ -0,0 +1,12 @@
+import { NextComponentType } from "next"
+import React from 'react'
+
+type Props = {}
+
+const LinkResult:NextComponentType = ({}: Props) => {
+  return (
+    <div>LinkResult</div>
+  )
+}
+
+export default LinkResult
