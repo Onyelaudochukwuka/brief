1d8d90385eac2fcd476200a97d15e85bb300556e
Udoka <udochukwukaonyela@gmail.com>
2022-07-16 20:34:48 +0100

    added domains for popular image providers

--- components/LinkShortener.tsx
+++ components/LinkShortener.tsx
@@ -28,7 +28,7 @@ const LinkShortener = ({ check }: Props) => {
               if (!linkVal) return false;
               if (!check) return setDisplayCustom(true);
           }} >
-          <div className="lg:w-1/2 w-4/5 flex items-center justify-center mx-auto mt-8 gap-6">
+          <div className="lg:w-1/2 w-4/5 flex lg:flex-row flex-col items-center justify-center mx-auto mt-8 gap-6">
               <label className="w-full relative block mx-auto">
                   <span className="absolute inset-y-0 left-2 flex items-center pl-2 justify-center">
                       <Image src={link} height="48" width="48" />
@@ -47,8 +47,8 @@ const LinkShortener = ({ check }: Props) => {
                   if (!linkVal) return false;
                   if (!check) return setDisplayCustom(true);
               }} >
-              <div className="lg:w-1/2 w-4/5 flex items-center justify-center mx-auto mt-8 gap-6">
-                  <span className="transition-all duration-500 ease-in bg-[transparent] w-full py-4 pl-20 pr-3 shadow-sm outline-none ring-2 focus:ring-neutral ring-dark ring-offset-dark ring-offset-1 focus:ring-offset-neutral/30 h-[auto] align-middle my-auto rounded-md text-light placeholder:text-light placeholder:text-lg text-lg placeholder:font-bold font-bold flex items-center justify-center"><label htmlFor="custom">https://brief.site/api/</label><input type={"text"} id="custom" className="bg-[transparent] outline-none border-b-2 focus:border-neutral border-dark h-[auto] align-middle my-auto rounded-sm text-light placeholder:text-light/50 placeholder:text-lg text-lg placeholder:font-bold font-bold w-fit" placeholder="Extension"/></span>
+              <div className="lg:w-1/2 w-4/5 flex lg:flex-row flex-col items-center justify-center mx-auto mt-8 gap-6">
+                  <div className="transition-all overflow-scroll duration-500 ease-in bg-[transparent] w-full py-4 pl-20 pr-3 shadow-sm outline-none ring-2 focus:ring-neutral ring-dark ring-offset-dark ring-offset-1 focus:ring-offset-neutral/30 h-[auto] align-middle my-auto rounded-md text-light placeholder:text-light placeholder:text-lg text-lg placeholder:font-bold font-bold flex items-center justify-center box-border"><label htmlFor="custom">https://brief.site/api/</label><input type={"text"} id="custom" className="bg-[transparent] outline-none border-b-2 focus:border-neutral border-dark h-[auto] align-middle my-auto rounded-sm text-light placeholder:text-light/50 placeholder:text-lg text-lg placeholder:font-bold font-bold w-auto" placeholder="Extension"/></div>
 
                   <button className="p-4 font-semibold rounded  bg-neutral text-light w-fit hover:shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] tracking-wide hover:bg-dark shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)]  transition-[box-shadow,background,color] duration-500 ease-in-out cursor-pointer active:scale-90" type="submit">Shorten</button>
               </div>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ['pbs.twimg.com'],
+    domains: ['pbs.twimg.com', "lh3.googleusercontent.com","avatars.githubusercontent.com"],
   },
   reactStrictMode: true,
 }
