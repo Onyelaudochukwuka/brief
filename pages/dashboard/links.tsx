1bff0507917e4e6cab4a874360552b5bb4c6298d
Udoka <udochukwukaonyela@gmail.com>
2022-07-16 23:22:12 +0100

    testing loader

--- pages/dashboard/links.tsx
+++ pages/dashboard/links.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { DashBoard } from "../../components";
+import Loader from "../../components/Loader";
 
 const links = () => {
   return (
     <div>
       <DashBoard />
-      
+      <Loader />
     </div>
   )
 }
