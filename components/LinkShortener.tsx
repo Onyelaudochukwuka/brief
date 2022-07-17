98b9eaa636618a9cccac39e267b7c45145448ed8
Udoka <udochukwukaonyela@gmail.com>
2022-07-17 13:41:08 +0100

    logic processing

--- components/LinkShortener.tsx
+++ components/LinkShortener.tsx
@@ -3,6 +3,7 @@ import { useSession } from "next-auth/react"
 import Image from "next/image"
 import React, { useEffect, useState } from 'react'
 import { link } from "../public"
+import { signedInShorten } from "../services"
 
 type Props = {
     check?: boolean;
@@ -12,6 +13,7 @@ type Props = {
 const LinkShortener = ({ check }: Props) => {
     const { data: session } = useSession();
     const [linkVal, setLinkVal] = useState('');
+    const [ext, setExt] = useState('');
     const [displayCustom, setDisplayCustom] = useState(false);
     useEffect(() => {
         if (check) {
@@ -41,13 +43,20 @@ const LinkShortener = ({ check }: Props) => {
 
        
               { displayCustom && <form action="#" className="w-full" onSubmit={(e) => {
-                  e.preventDefault();
-
-                  if (!linkVal) return false;
-                  if (!check) return setDisplayCustom(true);
+              e.preventDefault();
+              let obj = {
+                  longUrl: linkVal,
+                  email: session?.user?.email,
+                  customExt: ext,
+              };
+              signedInShorten(obj)
+                  .then((res) => console.log(res))
+                  .catch((err) => console.error(err));
               }} >
               <div className="lg:w-1/2 w-4/5 flex lg:flex-row flex-col items-center justify-center mx-auto mt-8 gap-6">
-                  <div className="transition-all overflow-scroll duration-500 ease-in bg-[transparent] w-full py-4 pl-20 pr-3 shadow-sm outline-none ring-2 focus:ring-neutral ring-dark ring-offset-dark ring-offset-1 focus:ring-offset-neutral/30 h-[auto] align-middle my-auto rounded-md text-light placeholder:text-light placeholder:text-lg text-lg placeholder:font-bold font-bold flex items-center justify-center box-border"><label htmlFor="custom">https://brief.site/api/</label><input type={"text"} id="custom" className="bg-[transparent] outline-none border-b-2 focus:border-neutral border-dark h-[auto] align-middle my-auto rounded-sm text-light placeholder:text-light/50 placeholder:text-lg text-lg placeholder:font-bold font-bold w-auto" placeholder="Extension"/></div>
+                  <div className="transition-all overflow-scroll duration-500 ease-in bg-[transparent] w-full py-4 pl-20 pr-3 shadow-sm outline-none ring-2 focus:ring-neutral ring-dark ring-offset-dark ring-offset-1 focus:ring-offset-neutral/30 h-[auto] align-middle my-auto rounded-md text-light placeholder:text-light placeholder:text-lg text-lg placeholder:font-bold font-bold flex items-center justify-center box-border">
+                      <label htmlFor="custom">https://brief.site/api/</label>
+                      <input type={"text"} value={ext} onChange={(e)=>setExt(e.target.value)} id="custom" className="bg-[transparent] outline-none border-b-2 focus:border-neutral border-dark h-[auto] align-middle my-auto rounded-sm text-light placeholder:text-light/50 placeholder:text-lg text-lg placeholder:font-bold font-bold w-auto" placeholder="Extension" /></div>
 
                   <button className="p-4 font-semibold rounded  bg-neutral text-light w-fit hover:shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] tracking-wide hover:bg-dark shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)]  transition-[box-shadow,background,color] duration-500 ease-in-out cursor-pointer active:scale-90" type="submit">Shorten</button>
               </div>
