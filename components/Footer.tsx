c92fb5651770ec9df12bd5e5f21eb1e928ad5ca1
Udoka <udochukwukaonyela@gmail.com>
2022-07-17 13:08:34 +0100

    removed company links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,28 +8,21 @@ const Footer: NextComponentType = () => {
   const ID = useId();
   return (
     <div className="flex flex-col w-full p-12 items-center text-light bg-navBar font-semibold mt-8 gap-4">
-      <div className="flex w-full lg:items-baseline py-4 lg:flex-row flex-col lg:gap-0 gap-6 items-start">
-        <span className="basis-2/5 text-center text-3xl align-middle my-auto font-bold">Brief</span>
-        <span className="flex flex-col lg:gap-4 gap-1 basis-1/5">
+      <div className="flex w-full lg:items-baseline py-4 lg:flex-row flex-col lg:gap-0 justify-center gap-6 items-start">
+        <span className="basis-1/3 text-center text-3xl align-middle my-auto font-bold">Brief</span>
+        <span className="flex flex-col lg:gap-4 gap-1 basis-1/3">
           <span className="font-bold text-lg">Features</span>
           <span className="flex flex-col gap-2">
           <span>Link Shortening</span>
           <span>Analytics</span>
         </span>
         </span>
-        <span className="flex flex-col lg:gap-4 gap-1 basis-1/5">
+        <span className="flex flex-col lg:gap-4 gap-1 basis-1/3">
           <span className="font-bold text-lg">Resources</span>
           <span className="flex flex-col gap-2">
           <span>Blog</span>
           <span>Developers</span>
           <span>Support</span>
-        </span>
-        </span>
-        <span className="flex flex-col lg:gap-4 gap-1 basis-1/5">
-          <span className="font-bold text-lg">Company</span>
-          <span className="flex flex-col gap-2">
-          <span>Link Shortening</span>
-          <span>Analytics</span>
         </span>
         </span>
       </div>
