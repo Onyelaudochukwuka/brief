c23397e01108285add85bb8951c3ce9463cd3248
Udoka <udochukwukaonyela@gmail.com>
2022-07-17 13:10:20 +0100

    removed test loader

--- pages/dashboard/links.tsx
+++ pages/dashboard/links.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { DashBoard } from "../../components";
-import Loader from "../../components/Loader";
 
 const links = () => {
   return (
     <div>
       <DashBoard />
-      <Loader />
     </div>
   )
 }
