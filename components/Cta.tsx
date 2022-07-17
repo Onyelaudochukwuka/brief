816f6fe9be821c964c319f6e909dd22a865a7b0c
Udoka <udochukwukaonyela@gmail.com>
2022-07-17 18:44:59 +0100

    added signIn link

--- components/Cta.tsx
+++ components/Cta.tsx
@@ -1,6 +1,7 @@
 import { NextComponentType } from "next"
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React, { useState } from 'react';
 import {arrow } from '../public';
 const data = [
@@ -17,7 +18,8 @@ const data = [
     details: "We offer fast link redirection, which means you don't have to wait for your page to load twice!"
   }
 ]
-const Cta:NextComponentType = () => {
+const Cta: NextComponentType = () => {
+  const { push, asPath } = useRouter();
   return (
     <div className="p-4 flex flex-col gap-10">
       <div>
@@ -42,7 +44,7 @@ const Cta:NextComponentType = () => {
       </div>
       <div className="flex flex-col text-center text-light items-center gap-8">
         <h1 className="tracking-wide text-xl font-bold">Get A Brief Link Today</h1>
-        <span className="px-4 py-2.5 rounded-lg bg-neutral text-light font-bold w-fit hover:shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] tracking-wide hover:bg-dark shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)]  transition-[box-shadow,background,color] duration-500 ease-in-out cursor-pointer"><Link href="/">Get Started</Link></span>
+        <span className="px-4 py-2.5 rounded-lg bg-neutral text-light font-bold w-fit hover:shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] tracking-wide hover:bg-dark shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)]  transition-[box-shadow,background,color] duration-500 ease-in-out cursor-pointer" onClick={() => push(`/auth/signIn?callbackUrl=${asPath}dashboard`)}>Get Started</span>
       </div>
     </div>
   )
