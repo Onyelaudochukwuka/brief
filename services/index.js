c4227bf1d2bf277f7e29a7d2e423679dd31bc83f
Udoka <udochukwukaonyela@gmail.com>
2022-07-17 13:29:25 +0100

    added middleware for sending request to server

--- services/index.js
+++ services/index.js
@@ -8,3 +8,13 @@ export const shortenLink = async (obj) => {
     });
     return result.json();
 }
+export const signedInShorten = async (obj) => {
+    const result = await fetch('/api/signinshorten', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(obj),
+    });
+    return result.json();
+}
