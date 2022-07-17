71b9f75660955fb8cd97363ba23619a56a764489
Udoka <udochukwukaonyela@gmail.com>
2022-07-17 12:56:08 +0100

    shortened link

--- components/Shorten.tsx
+++ components/Shorten.tsx
@@ -1,7 +1,7 @@
 import { NextComponentType } from "next";
 import React, { useState } from 'react'
 import { shortenLink } from '../services';
-import LinkResult from "./LinkResult";
+import Result from "./Result";
 const Shorten:NextComponentType = () => {
   const [longUrl, setLongUrl] = useState('');
   const [data, setData] = useState({});
@@ -24,7 +24,7 @@ const Shorten:NextComponentType = () => {
         <input value={longUrl} onChange={(e) => setLongUrl(e.target.value)} type="url" placeholder="https://brief.dev/E8bCl293" className="basis-3/4 bg-transparent border-b-2 border-b-neutral focus:border-0 focus:border-b-2 focus:ring-0 text-lg text-light"/>
         <button type="submit" className="px-6 py-3 text-lg outline-0 focus:outline-0 focus:border-0  rounded-lg font-bold bg-neutral text-light w-fit hover:shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] tracking-wide hover:bg-dark shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)]  transition-[box-shadow,background,color] duration-500 ease-in-out cursor-pointer active:scale-90">{load ? <div className="w-2 h-2 animate-bounce bg-light mx-6 my-3"></div> : "Shorten"}</button>
       </form>
-      {showData && <LinkResult data={data} />}
+      {showData && <Result data={data} />}
     </>
   )
 }
