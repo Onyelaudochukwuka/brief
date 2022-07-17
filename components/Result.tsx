582a7a5a1cf46e50f1b1d72cc321c87add63bb1c
Udoka <udochukwukaonyela@gmail.com>
2022-07-17 13:00:37 +0100

    added a target attribute to link

--- components/Result.tsx
+++ components/Result.tsx
@@ -18,7 +18,7 @@ const Result = ({ data }: Props) => {
       <div className="linear w-fit max-w-[83%] p-8 px-6 mt-4 font-bold text-light tracking-widest flex  lg:flex-row flex-col items-center gap-6 text-sm justify-center mx-auto">
                 <p className="break-all basis-3/5">{ data?.longUrl }</p>
                 <span className="flex lg:flex-row flex-col w-full text-center basis-2/5 items-center justify-center gap-3">
-                    <a href={data?.shortUrl} className="text-[#38bdf8] hover:text-[#38bdf8]/40 transition duration-500 ease-in">{data?.shortUrl}</a>
+                    <a href={data?.shortUrl} target={"_blank"} className="text-[#38bdf8] hover:text-[#38bdf8]/40 transition duration-500 ease-in">{data?.shortUrl}</a>
                     <span className="w-full lg:w-fit text-center text-neutral bg-neutral/30 p-3 rounded-md hover:bg-neutral hover:text-light transition duration-500 ease-in cursor-pointer" onClick={() => {
                     setCopied(true);
                     setTimeout(() => setCopied(false), 3000)
