ca7427cd816bc19796344167a17f1b82239630bb
Udoka <udochukwukaonyela@gmail.com>
2022-07-17 12:52:58 +0100

    page loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -4,8 +4,7 @@ type Props = {}
 
 const Loader = (props: Props) => {
   return (
-      <div className="border-3 ">
-          
+      <div className="border-2 rounded-full border-light w-8 h-8 border-b-0 border-l-0 animate-spin animate-ping">
     </div>
   )
 }
