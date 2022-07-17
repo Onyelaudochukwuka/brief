5f5566f5ef18ab75343d5542e139ef79015a0bb7
Udoka <udochukwukaonyela@gmail.com>
2022-07-17 19:50:18 +0100

    styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,34 +13,25 @@ const Navbar: NextComponentType = () => {
     const data = await signOut({ redirect: false, callbackUrl: "/" });
     push(data.url);
   };
-  console.log(session);
   return (
-    <div className="flex w-full p-6 items-center text-light bg-navBar font-semibold">
+    <div className="flex w-full p-6 justify-between items-center text-light bg-navBar font-semibold">
       <div className="basis-2/5">
         <div className="w-24 h-fit">
           <Image src={logo} layout="responsive" />
         </div>
-      </div>
-      <div className={`hidden lg:flex basis-3/5 w-full items-center`}>
-        <div className="flex w-full basis-3/5">
-          <span className="basis-1/3 hover:text-neutral transition-all duration-500 ease-in-out">
-            <Link href="/">Why briefly</Link>
+      </div> 
+      <div className={`lg:flex ${drop ? `flex flex-col absolute gap-4 top-24 z-20 w-full bg-dark left-0 py-4` : `hidden`} lg:flex-row lg:relative justify-between w-full items-center`}>
+          <span className=" hover:text-neutral transition-all duration-500 ease-in-out">
+            <a href="#features">Products</a>
           </span>
-          <span className="basis-1/3 hover:text-neutral transition-all duration-500 ease-in-out">
-            <Link href="/">Products</Link>
+          <span className=" hover:text-neutral transition-all duration-500 ease-in-out">
+            <Link href="/features">Resources</Link>
           </span>
-          <span className="basis-1/3 hover:text-neutral transition-all duration-500 ease-in-out">
-            <Link href="/">Resources</Link>
-          </span>
-        </div>
-        <div className="flex basis-2/5 gap-6 items-center ml-6">
           <span className="bg-neutral py-3 px-6 rounded-full shadow-[0_4px_4px_1px_rgba(4,92,148,0.45)] active:scale-95 hover:bg-[#4097CE] hover:shadow-[0_5px_4px_0px_rgba(64,151,206,0.75)] active:shadow-[0_6px_4px_1px_rgba(64,151,206,0.75)] cursor-pointer transition-all duration-500 ease-in-out" onClick={() => push(`/auth/signIn?callbackUrl=${asPath}dashboard`)}>
             Login
           </span>
-        </div>
       </div>
-      <span onClick={handleSignOut}>SignOut</span>
-      <div className="lg:hidden float-right ml-auto">
+      <div className="lg:hidden float-right ml-auto" onClick={()=>setDrop(!drop)}>
         <div className="w-14 h-1 bg-light rounded-full cursor-pointer
         after:w-14 after:h-1 after:bg-light after:rounded-full after:absolute after:-mt-2.5 after:cursor-pointer
         before:w-14 before:h-1 before:bg-light before:rounded-full before:absolute before:mt-2.5 before:cursor-pointer"></div>
