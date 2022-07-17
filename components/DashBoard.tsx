643448131846d41f55e893ed4e6b70f4db416c43
Udoka <udochukwukaonyela@gmail.com>
2022-07-17 13:08:24 +0100

    added ping animation

--- components/DashBoard.tsx
+++ components/DashBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { NextComponentType } from 'next';
 import { useSession } from "next-auth/react";
 import Image from "next/image";
@@ -14,13 +14,18 @@ const DashBoard: NextComponentType = ({ }: Props) => {
       push('/auth/signIn')
     },
   });
+  const [pulse, setPulse] = useState(true)
+  useEffect(() => {
+  setTimeout(()=>setPulse(false),2500)
+  }, [])
+  
   console.log(session);
   const [check, setCheck] = useState(false);
   return (
   <>
     <div className="flex p-6 bg-navBar justify-between">
       <div className={`w-max ${check ? `bg-neutral` : `bg-lightNeutral`} rounded-full h-10 flex items-center justify-center gap-1 transition ${check && `flex-row-reverse`} duration-500  px-1 py-6`}>
-        <div className={`w-14 h-10 rounded-full bg-light cursor-pointer transition-[order] duration-300 `} onClick={()=>setCheck(!check)}></div>
+        <div className={`w-14 h-10 rounded-full bg-light cursor-pointer transition-[order] duration-300 ${pulse && `animate-pulse`}`} onClick={()=>setCheck(!check)}></div>
         <span className="text-lg font-bold transition-all duration-500">{check ? "Default" : "Custom"}</span>
       </div> 
         {session && <div className="cursor-pointer rounded-full"><Image className="rounded-full" priority src={session?.user?.image ? session?.user?.image : userIcon} width="48px" height={"48px"} /></div>}
