369eb4ede6983af9c15e2b1f4648e258de0e4ac9
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2022-07-18 16:16:21 +0100

    Fixed typo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero: NextComponentType = () => {
       <div className="lg:basis-1/2 flex lg:items-baseline flex-col">
         <div className="basis-1/2">
           <h1 className="lg:text-[44px] text-2xl font-bold">Brief Links, Faster Results</h1>
-          <p className="font-semibold lg:text-[28px] text-base leading-relaxed">An URL Shortener Built With Tools To simplify Things.</p>
+          <p className="font-semibold lg:text-[28px] text-base leading-relaxed">An URL Shortener Built To simplify Things.</p>
         </div>
         <div className="basis-1/2 mt-16">
           <span className="lg:p-6  p-4 rounded-xl shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] bg-dark font-bold tracking-wide hover:bg-neutral hover:shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)] transition-all duration-500 ease-in-out cursor-pointer" onClick={() => push(`/auth/signIn?callbackUrl=${asPath}dashboard`)}>Get Started</span>
