8c4c53984dbaf40bf978a58239141760ac47280a
Udoka <udochukwukaonyela@gmail.com>
2022-07-18 13:47:56 +0100

    fixes

--- pages/api/auth/lib/mongodb.js
+++ pages/api/auth/lib/mongodb.js
@@ -1,15 +1,14 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 require('dotenv').config();
 import { MongoClient } from "mongodb";
-const MONGODB_PARAMS = process.env.MONGODB_USERNAME && process.env.MONGODB_PASSWORD;
-const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}$1@cluster0.bmcxh.mongodb.net/auth?retryWrites=true&w=majority`;
+const uri = process.env.MONGODB_URI;
 const options = {
     useUnifiedTopology: true,
     useNewUrlParser: true,
 }
 
-if (!MONGODB_PARAMS) {
-    throw new Error("Please add your MONGODB_USERNAME and MONGODB_PASSWORD to .env.local")
+if (!uri) {
+    throw new Error("uri missing")
 }
 
 let client = new MongoClient(uri, options);
