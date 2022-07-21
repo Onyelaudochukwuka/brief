c6f6ebd7fc65d0c7563e032c8a0109ecc15347c1
Udoka <udochukwukaonyela@gmail.com>
2022-07-21 19:09:50 +0100

    aded a loader to all buttons

--- components/LinkShortener.tsx
+++ components/LinkShortener.tsx
@@ -19,6 +19,7 @@ const LinkShortener = ({ check }: Props) => {
     const [error, setError] = useState(false);
     const [data, setData] = useState({});
     const [showData, setShowData] = useState(false);
+    const [load, setLoad] = useState(false)
     useEffect(() => {
         if (check) {
             setShowData(false)
@@ -33,11 +34,14 @@ const LinkShortener = ({ check }: Props) => {
               e.preventDefault();
               if (!linkVal) return false;
               if (!check) return setDisplayCustom(true);
+              setLoad(true);
               return signedInShorten({ longUrl: linkVal, email: session?.user?.email })
                   .then(res => {
                       setShowData(true);
                       return setData(res)
-              })
+              }).then(()=>setTimeout(() => {
+                  setLoad(false);
+              }, 1000))
           }} >
           <div className="lg:w-1/2 w-4/5 flex lg:flex-row flex-col items-center justify-center mx-auto mt-8 gap-6">
               <label className="w-full relative block mx-auto">
@@ -47,7 +51,7 @@ const LinkShortener = ({ check }: Props) => {
                   <input type="url" className="block transition-all duration-500 ease-in bg-[transparent] w-full py-4 pl-20 pr-3 shadow-sm outline-none ring-2 focus:ring-neutral ring-dark ring-offset-dark ring-offset-1 focus:ring-offset-neutral/30 h-[auto] align-middle my-auto rounded-md text-light placeholder:text-light placeholder:text-lg text-lg placeholder:font-bold font-bold placeholder:align-middle placeholder:my-auto" placeholder="Enter Link Here" value={linkVal} onChange={(e)=>setLinkVal(e.target.value)} />
               </label>
 
-                  <button className="p-4 font-semibold rounded  bg-neutral text-light lg:w-fit w-full hover:shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] tracking-wide hover:bg-dark shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)]  transition-[box-shadow,background,color] duration-500 ease-in-out cursor-pointer active:scale-90" type="submit">{check ? "Shorten" : "Continue"}</button>
+                  <button className="p-4 font-semibold rounded  bg-neutral text-light lg:w-fit w-full hover:shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] tracking-wide hover:bg-dark shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)]  transition-[box-shadow,background,color] duration-500 ease-in-out cursor-pointer active:scale-90" type="submit" disabled={check ? load ? true : false : false}>{check ?  load ? <div className = "w-2 h-2 animate-bounce bg-light mx-6 my-3"></div> : "Shorten" : "Continue"}</button>
               </div>
           </form>
 
@@ -75,7 +79,7 @@ const LinkShortener = ({ check }: Props) => {
                       <label htmlFor="custom">https://brief.site/api/</label>
                       <input type={"text"} value={ext} onChange={(e)=>setExt(e.target.value)} id="custom" className="bg-[transparent] outline-none border-b-2 focus:border-neutral border-dark h-[auto] align-middle my-auto rounded-sm text-light placeholder:text-light/50 placeholder:text-lg text-lg placeholder:font-bold font-bold w-auto" placeholder="Extension" /></div>
 
-                  <button className="p-4 font-semibold rounded bg-neutral text-light lg:w-fit w-full hover:shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] tracking-wide hover:bg-dark shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)]  transition-[box-shadow,background,color] duration-500 ease-in-out cursor-pointer active:scale-90" type="submit">Shorten</button>
+                  <button type="submit" className="px-6 py-3 text-lg outline-0 focus:outline-0 focus:border-0  rounded-lg font-bold bg-neutral text-light hover:shadow-[3px_4px_2px_rgba(4,92,148,0.25),_0px_4px_2px_rgba(4,92,148,0.25),_4px_4px_2px_rgba(4,92,148,0.73),_3px_3px_2px_rgba(4,92,148,0.288)] tracking-wide hover:bg-dark shadow-[3px_4px_2px_rgba(2,49,77,0.25),_0px_4px_2px_rgba(2,49,77,0.25),_4px_4px_2px_rgba(2,49,77,0.73),_3px_3px_2px_rgba(2,49,77,0.288)]  transition-[box-shadow,background,color] duration-500 ease-in-out cursor-pointer active:scale-90 lg:w-fit w-full" disabled={load ? true : false}>{load ? <div className="w-2 h-2 animate-bounce bg-light mx-6 my-3"></div> : "Shorten"}</button>
               </div>
               </form>
           }
