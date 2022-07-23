0ea0528f82b79aac5845989beb1777d206b8e306
Udoka <udochukwukaonyela@gmail.com>
2022-07-23 11:20:14 +0100

    add-ons

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ module.exports = {
     domains: ['pbs.twimg.com', "lh3.googleusercontent.com","avatars.githubusercontent.com"],
   },
   reactStrictMode: true,
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en-US'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'en-US'
+  }
 }
